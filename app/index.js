f319d1c121bd3ed28889d98466ed12fbe0427614
DS <dsrathorebsw1234@gmail.com>
2024-02-09 15:54:42 +0530

    add page picker

--- app/index.js
+++ app/index.js
@@ -2,38 +2,43 @@ import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import "../global.css";
 import { Link, Stack } from "expo-router";
 import { Feather } from '@expo/vector-icons';
-import Form from "./Form/Form.jsx";
-import Done from "./Done/Done.jsx";
+import PagerView from 'react-native-pager-view';
 
 export default function Page() {
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{
-        title: ""
+        title: "",
+        headerShadowVisible: false
       }} />
-
-      <View className="relative h-screen w-full" style={styles.main}>
-        <Text className="text-[60px] mb-24 -mt-8 uppercase text-center text-white font-black">Wel<Text className="text-[#86A789]">come</Text> to <Text className="text-[#86A789]">Work</Text>ino
-        </Text>
-        <Text className='text-[16px] text-center text-[#86A789] font-medium uppercase'>Discover now</Text>
-        <Text className='text-[25px] text-center text-white font-bold uppercase mt-2'>Find your on-demand service worker</Text>
-        <Text className='text-[14px] text-center w-3/4 text-white font-base mt-5'>We provide better service for you with our <Text className="font-bold text-[#86A789]">On-Demand service</Text> app.
-        </Text>
-        {/* <View className="absolute top-56 left-2 h-16 w-32 rounded-xl bg-[#ffffff59] justify-center items-center">
-          <Text className="text-[14px] text-[#D2E3C8]">On-Demand</Text>
+      <PagerView style={{ flex: 1, }} initialPage={0}>
+        <View className="relative h-screen w-full flex flex-col justify-center items-center" style={styles.main} key="1">
+          <Text className="text-[60px] mb-24 -mt-8 uppercase text-center text-white font-black">Wel<Text className="text-[#86A789]">come</Text> to <Text className="text-[#86A789]">Work</Text>ino
+          </Text>
         </View>
-        <View className="absolute right-2 top-64 h-16 w-32 rounded-xl bg-[#ffffff59] justify-center items-center">
-          <Text className="text-[14px] text-[#D2E3C8]">Hour Basis</Text>
-        </View> */}
-        <Pressable className="rounded-full absolute bottom-12 rounded-b-full uppercase active:translate-y-2 duration-200 ease-in-out transition-all bg-[#4F6F52] text-white w-20 h-20 text-3xl justify-center items-center">
-          <Link href="/home/HomePage" className="text-3xl text-white text-center">
-            <Feather name="arrow-up-right" size={30} color="white" />
-          </Link>
-        </Pressable>
-      </View>
+        <View className="relative h-screen w-full" style={styles.main} key="2">
+          <Text className='text-[16px] text-center text-[#86A789] font-medium uppercase'>Discover now</Text>
+          <Text className='text-[25px] text-center text-white font-bold uppercase mt-2'>Find your on-demand service worker</Text>
+          <Text className='text-[18px] text-center w-3/4 text-white font-base mt-5'>We provide better service for you with our <Text className="font-bold text-[#86A789]">On-Demand service</Text> app.
+          </Text>
+        </View>
+        <View className="relative h-screen w-full" style={styles.main} key="3">
+          <Text className="text-[60px] mb-24 -mt-8 uppercase text-center text-white font-black">Wel<Text className="text-[#86A789]">come</Text> to <Text className="text-[#86A789]">Work</Text>ino
+          </Text>
+          <Text className='text-[16px] text-center text-[#86A789] font-medium uppercase'>Discover now</Text>
+          <Text className='text-[25px] text-center text-white font-bold uppercase mt-2'>Find your on-demand service worker</Text>
+          <Text className='text-[14px] text-center w-3/4 text-white font-base mt-5'>We provide better service for you with our <Text className="font-bold text-[#86A789]">On-Demand service</Text> app.
+          </Text>
+          <Pressable className="rounded-full absolute bottom-32 rounded-b-full uppercase active:translate-y-2 duration-200 ease-in-out transition-all bg-[#4F6F52] text-white w-20 h-20 text-3xl justify-center items-center">
+
+            <Link href="/home/HomePage" className="text-3xl text-white text-center">
+              <Feather name="arrow-up-right" size={30} color="white" />
+            </Link>
+          </Pressable>
+        </View>
+      </PagerView>
+      <Text className="text-white">slide left ar</Text>
     </SafeAreaView>
-    // <Form />
-    // <Done />
   );
 }
 
@@ -47,7 +52,7 @@ const styles = StyleSheet.create({
     justifyContent: "start",
     marginHorizontal: "auto",
     alignItems: "center",
-    paddingTop: 30
+    paddingTop: 30,
   },
   img: {
     height: "80%",

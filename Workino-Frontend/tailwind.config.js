3747b0b54287542538d8e5df88502e103eecde65
DS <dsrathorebsw1234@gmail.com>
2024-03-05 16:29:01 +0530

    .gitignore

--- Workino-Frontend/tailwind.config.js
+++ Workino-Frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}",'./public/*'],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
